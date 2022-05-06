a3a6b8b155ee9eb6fe611929a878f6030030dec9
lordmaster11 <jmdicostanzo11@gmail.com>
2022-05-06 11:07:36 -0300

    congiguration new base date

---
+++ src/main/resources/data.sql
@@ -0,0 +1,75 @@
+insert into user (id, mail, name, password) values (1, 'Cesar', 'perez.cesaar@gmail.com', '1234');
+insert into user (id, mail, name, password) values (2, 'Marcelo', 'jmdicostanzo11@gmail.com', '1234');
+
+
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (1, 'dominadas australianas', 'ejercicio colgado', 'video1', 1, 'espalda');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (2, 'fondos', 'ejercicio en paralelas', 'video2', 1, 'triceps');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (3, 'flexiones', 'ejercicio en suelo', 'video3', 1, 'pectoral');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (4, 'sentadillas', 'ejercicio en suelo', 'video4', 1, 'piernas');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (5, 'dominadas supinas', 'ejercicio colgado', 'video5', 3, 'espalda');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (6, 'flexiones diamantes', 'ejercicio en suelo', 'video6', 3, 'pectoral');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (7, 'zancadas', 'ejercicio en suelo', 'video7', 3, 'piernas');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (8, 'plancha', 'ejercicio en suelo', 'video8', 3, 'abdominales');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (9, 'burpees', 'ejercicio en suelo', 'video9', 3, 'abdominales');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (10, 'flexiones a pino', 'ejercicio en suelo', 'video10', 5, 'pectoral');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (11, 'fondos explosivos', 'ejercicio en paralelas', 'video11', 5, 'triceps');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (12, 'muscle up', 'ejercicio colgado', 'video12', 5, 'hombros');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (13, 'pistol squat', 'ejercicio en suelo', 'video13', 5, 'piernas');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (14, 'dominadas pronas', 'ejercicio colgado', 'video14', 5, 'espalda');
+insert into processexercise (id, name_exercise, description, url_video, complexity_number, main_muscle) values (15, 'dragon flag', 'ejercicio en suelo', 'video15', 5, 'espalda');
+ 
+
+
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (25, 2, 'principiante', 30, 1, 'APP', 2);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (10, 4, 'principiante', 30, 1, 'APP', 3);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (15, 3, 'principiante', 30, 1, 'APP', 1);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (8, 5, 'principiante', 30, 1, 'APP', 4);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (3, 5, 'principiante', 30, 1, 'APP', 7);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (6, 6, 'principiante', 30, 1, 'APP', 6);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (5, 6, 'principiante', 30, 1, 'APP', 8);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (20, 3, 'principiante', 30, 1, 'APP', 9);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (12, 4, 'principiante', 30, 1, 'APP', 5);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (15, 4, 'principiante', 30, 1, 'APP', 11);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (20, 4, 'principiante', 30, 1, 'APP', 15);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (12, 3, 'principiante', 30, 1, 'APP', 10); 
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (8, 5, 'principiante', 30, 1, 'APP', 12);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (15, 3, 'principiante', 30, 1, 'APP', 14);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (10, 4, 'principiante', 30, 1, 'APP', 13);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (100, 2, 'principiante', 30, 1, 'APP', 4);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (50, 2, 'principiante', 30, 1, 'APP', 9);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (100, 2, 'principiante', 30, 1, 'APP', 3);
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (50, 2, 'principiante', 30, 1, 'APP', 14);  
+insert into exercise (repetitions, series, level_excercise, exercise_time, break_time, generated_by, process_exercise_id) values (50, 2, 'principiante', 30, 1, 'APP', 11);
+
+
+insert into routines (dtype, name_routine, generated_by, level) values ('RoutineByLevel', 'Rutina Principiantes', 'APP', 1);
+
+insert into routines_exercises (routine_id, exercises_id) values (1, 1);
+insert into routines_exercises (routine_id, exercises_id) values (1, 2);
+insert into routines_exercises (routine_id, exercises_id) values (1, 3);
+insert into routines_exercises (routine_id, exercises_id) values (1, 4);
+
+insert into routines (dtype, name_routine, generated_by, level) values ('RoutineByLevel', 'Rutina Intermedios', 'APP', 2);
+
+insert into routines_exercises (routine_id, exercises_id) values (2, 5);
+insert into routines_exercises (routine_id, exercises_id) values (2, 6);
+insert into routines_exercises (routine_id, exercises_id) values (2, 7);
+insert into routines_exercises (routine_id, exercises_id) values (2, 8);
+insert into routines_exercises (routine_id, exercises_id) values (2, 9);
+
+insert into routines (dtype, name_routine, generated_by, level) values ('RoutineByLevel', 'Rutina Avanzados', 'APP', 3);
+
+insert into routines_exercises (routine_id, exercises_id) values (3, 10);
+insert into routines_exercises (routine_id, exercises_id) values (3, 11);
+insert into routines_exercises (routine_id, exercises_id) values (3, 12);
+insert into routines_exercises (routine_id, exercises_id) values (3, 13);
+insert into routines_exercises (routine_id, exercises_id) values (3, 14);
+insert into routines_exercises (routine_id, exercises_id) values (3, 15);
+
+insert into routines (dtype, name_routine, generated_by, level) values ('RoutineByLevel', 'Rutina Espartana', 'APP', 3);
+
+insert into routines_exercises (routine_id, exercises_id) values (4, 16);
+insert into routines_exercises (routine_id, exercises_id) values (4, 17);
+insert into routines_exercises (routine_id, exercises_id) values (4, 18);
+insert into routines_exercises (routine_id, exercises_id) values (4, 19);
+insert into routines_exercises (routine_id, exercises_id) values (4, 20);
